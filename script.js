ac909457b6947f691f13f1848cb01b0934adec2c
Irina Mishchenko <i.mishchenko95@gmail.com>
2024-02-08 14:36:58 +0200

    video slider

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
